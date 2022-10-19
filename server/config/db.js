c08a7212fd580416e25c1ba2a70dd8bbcbc874a7
Sandip <35768586+bigsandip@users.noreply.github.com>
2022-10-19 19:09:23 -0400

    database

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-module.exports = 
-{
-    "URI": "mongodb://localhost/user_store"
-}
+module.exports = {
+    //URI: "mongodb://127.0.0.1:27017/book_store",
+    URI: "mongodb+srv://bigsandip:fHwE1ccKtSBMEp2a@sandipscluster.lk2cpz3.mongodb.net/?retryWrites=true&w=majority",
+  };
